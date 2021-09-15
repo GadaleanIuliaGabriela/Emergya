a3d6c59e4baa221c0c29dd23103fa3865b3c6f4d
Iulia Gadalean <gadalean.iulia.gabriela@gmail.com>
2021-09-16 00:51:32 +0300

    Add docker, view, content type fixes. Generate some default content.

--- Dockerfile
+++ Dockerfile
@@ -14,17 +14,15 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
 	netcat \
 	wget
 
+RUN pecl install xdebug
+
 RUN php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && \
   php composer-setup.php && \
   mv composer.phar /usr/local/bin/composer && \
   php -r "unlink('composer-setup.php');"
 
-RUN composer global require drush/drush:10.* && composer global install && ln -s /var/www/.config/composer/vendor/bin/drush /usr/bin/drush
+RUN composer global require drush/drush:10.* && composer global install && ln -s ~/.composer/vendor/bin/drush /usr/bin/drush
 
 RUN rm -rf /var/www/html/*
 
-COPY apache-drupal.conf /etc/apache2/sites-enabled/000-default.conf
-
-WORKDIR /var/www/html/web
-
-CMD ["sh", "-c", "/var/www/html/scripts/install.sh && apache2-foreground"]
+CMD ["sh", "-c", "/opt/drupal/scripts/install.sh && apache2-foreground"]
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-composer install -y
+composer install --no-interaction
 
 # Waits until mysql container is installed
 until nc -z -v -w1 db 3306
@@ -12,15 +12,17 @@ done
 if [[ "${SITE_INSTALL}" == "1" ]]; then
     printf "\n- Installing site -\n\n"
     drush site-install -y test --db-url=mysql://root:drupal@db:3306/drupal --site-name=DrupalProject --account-name=admin --account-pass=admin --account-mail=gadalean.iulia.gabriela@gmail.com
+    drush cset system.site uuid "29ad5c9a-0f76-4799-b8ac-5cd09c743191"
+    drush entity:delete shortcut_set
+    drush cset language.entity.en uuid "b6f99eeb-3ec8-4f40-a067-91f03e03d42b"
     printf "\n- Site has been successfully installed -\n"
 fi
 
 # Install xdebug optionally
 if [[ "${XDEBUG_INSTALL}" == "1" ]]; then
     printf "\n- Installing xdebug -\n\n"
-    pecl install xdebug
     docker-php-ext-enable xdebug
-    cat ../docker/configs/xdebug.ini > /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+    cat ./docker/configs/xdebug.ini > /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
     printf "\n- Xdebug has been successfully installed -\n"
 fi
 
@@ -28,5 +30,8 @@ drush cr
 drush -y updb
 drush -y cim --partial
 drush cr
+
+# Generate some content
+drush genc 10 --bundles product
 
 chown -R www-data:www-data /var/www
