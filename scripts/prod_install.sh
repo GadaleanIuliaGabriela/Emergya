be52d0475624072b6720754d2df7af487de0c6b0
Iulia Gadalean <gadalean.iulia.gabriela@gmail.com>
2021-09-30 11:04:02 +0300

    Update prod_install.sh.

--- scripts/prod_install.sh
+++ scripts/prod_install.sh
@@ -4,9 +4,9 @@
 if [[ "${SITE_INSTALL}" == "1" ]]; then
   printf "\n- Installing site -\n\n"
   drush site-install -y test --db-url=pgsql://yfkjrtmbazvxqo:93c526e7d95308f024ba0969393ab423f25eb5615624b124e8cfcccb969fd2cd@ec2-54-74-77-126.eu-west-1.compute.amazonaws.com:5432/da7ktgieemu0q2 --site-name=DrupalProject --account-name=admin --account-pass=admin --account-mail=gadalean.iulia.gabriela@gmail.com
-  drush cset system.site uuid "29ad5c9a-0f76-4799-b8ac-5cd09c743191"
-  drush entity:delete shortcut_set
-  drush cset language.entity.en uuid "b6f99eeb-3ec8-4f40-a067-91f03e03d42b"
+  drush cset -y system.site uuid "29ad5c9a-0f76-4799-b8ac-5cd09c743191"
+  drush entity:delete -y shortcut_set
+  drush cset -y language.entity.en uuid "b6f99eeb-3ec8-4f40-a067-91f03e03d42b"
   printf "\n- Site has been successfully installed -\n"
 fi
 
